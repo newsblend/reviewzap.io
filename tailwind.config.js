3611d447e4e4ce9ba07a3cb4aea52bc4590325bc
Jakob de Maeyer <jakobdm1@gmail.com>
2023-05-27 13:04:38 +0200

    Refactor everything, add legal pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.html",
+    "_includes/*.html",
+    "_layouts/*.html",
+  ],
+  theme: {
+    fontFamily: {
+      'display': 'Raleway, Oswald, ui-sans-serif, system-ui, -apply-system, sans-serif',
+      'body': 'Cantarell, ui-sans-serif, system-ui, -apple-system, sans-serif',
+    },
+  },
+  plugins: [
+  ],
+}
